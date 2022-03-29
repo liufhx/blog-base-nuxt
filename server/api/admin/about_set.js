70ad49dbd31dd8445cc122b225d05d85b37d926c
liufhx <2814482535@qq.com>
2022-03-29 17:46:10 +0800

    🐞 fix: 修复文件保存路径错误
    修复更改page.json文件后保存的路径错误

--- server/api/admin/about_set.js
+++ server/api/admin/about_set.js
@@ -9,7 +9,7 @@ module.exports=(req,res)=>{
     const PAGE=fs.readFileSync(PageDir,'utf-8')
     let page=JSON.parse(PAGE)
     page=JSON.stringify({...page,about:{_id}})
-    fs.writeFileSync('../page.json',page)
+    fs.writeFileSync(PageDir,page)
     res.send(preRes('设置about页面_id成功',200))
   }catch(err){
     res.send(preRes('设置about页面_id失败,因为'+err.message,500))
