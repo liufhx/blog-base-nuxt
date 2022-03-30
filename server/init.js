88196ae7eb4508b4ba338e65fa8943ef102dfbe5
liufhx <2814482535@qq.com>
2022-03-31 00:17:18 +0800

    🦄 refactor: 修复创建多级目录失败的问题

--- server/init.js
+++ server/init.js
@@ -3,18 +3,21 @@ const fs=require('fs')
 const {User}=require('./model/user')
 const {admin}=require('../config').server.user
 const hash=require('./util/hash')
+const mkdirp=require('mkdirp')
 
 
   
 //创建图片上传文件夹
-let dir=path.join(__dirname,'../','public','upload','cover')
-if(!fs.existsSync(dir)){
-  fs.mkdir(dir,err=>{
-    if(err!=null){
-      console.log(err)
-    }
-  })
+let coverDir=path.join(__dirname,'../','public','upload','cover')
+try{
+  if(!fs.existsSync(coverDir)){
+    mkdirp.sync(coverDir)
+  }
+}catch(err){
+  console.log(err.message)
 }
+
+
 
 //创建页面数据文件
 let pageDir=path.join(__dirname,'../','page.json')
@@ -23,7 +26,7 @@ if(!fs.existsSync(pageDir)){
   let page=JSON.stringify({initDate})
   fs.writeFile(pageDir,page,err=>{
     if(err!=null){
-      console.log(err)
+      console.log(err.message)
     }
   })
 }
